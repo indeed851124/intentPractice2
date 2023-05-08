749f0b79c1e4f6b4d454a7bfd21cc6885c7fd215
LKH <kangholee8512@gmail.com>
2023-05-09 06:04:34 +0900

    Initial commit

---
+++ app/src/main/java/com/cnc/intentpractice/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.cnc.intentpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cnc.intentpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var mBinding: ActivityMainBinding? = null
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
